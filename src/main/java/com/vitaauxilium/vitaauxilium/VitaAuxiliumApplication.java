4bd3c18212bb7bedf6829740e864af34c3058b70
Ryan-Barros <ryanbarros975@gmail.com>
2026-04-22 19:49:43 -0300

    commit inicial

---
+++ src/main/java/com/vitaauxilium/vitaauxilium/VitaAuxiliumApplication.java
@@ -0,0 +1,13 @@
+package com.vitaauxilium.vitaauxilium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaAuxiliumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitaAuxiliumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vitaauxilium/vitaauxilium/VitaAuxiliumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitaauxilium.vitaauxilium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaAuxiliumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
