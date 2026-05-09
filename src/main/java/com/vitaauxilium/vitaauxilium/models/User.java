21c894074033e9bd75ba06a0f257104f92ad796c
Ryan-Barros <ryanbarros975@gmail.com>
2026-05-09 19:25:16 -0300

    Cria o model de User

---
+++ src/main/java/com/vitaauxilium/vitaauxilium/models/User.java
@@ -0,0 +1,62 @@
+package com.vitaauxilium.vitaauxilium.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID id;
+
+    @Column(name = "user_name", nullable = false, length = 45)
+    private String name;
+
+    @Column(name = "user_phone", nullable = false, length = 11)
+    private String phone;
+
+    @Column(name = "user_email", nullable = false, length = 100, unique = true)
+    private String email;
+
+    @Column(name = "user_password", nullable = true, columnDefinition = "TEXT")
+    private String password;
+
+    @Column(name = "active", nullable = false)
+    private boolean active = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
