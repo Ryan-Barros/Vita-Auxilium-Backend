0fccb374040db800520e258cbcd2aab39cbb74c4
Ryan-Barros <ryanbarros975@gmail.com>
2026-05-09 19:28:37 -0300

    Implementa o service de User

---
+++ src/main/java/com/vitaauxilium/vitaauxilium/services/UserService.java
@@ -0,0 +1,45 @@
+package com.vitaauxilium.vitaauxilium.services;
+
+import com.vitaauxilium.vitaauxilium.dto.request.UserRequestDTO;
+import com.vitaauxilium.vitaauxilium.dto.request.UserUpdateDTO;
+import com.vitaauxilium.vitaauxilium.dto.response.UserResponseDTO;
+import com.vitaauxilium.vitaauxilium.mapper.UserMapper;
+import com.vitaauxilium.vitaauxilium.models.User;
+import com.vitaauxilium.vitaauxilium.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public List<UserResponseDTO> findAll() {
+        List<User> users = userRepository.findAll();
+        return userMapper.toResponseDTOList(users);
+    }
+
+    public UserResponseDTO findById(UUID id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado!"));
+        return userMapper.toResponseDTO(user);
+    }
+
+    public UserResponseDTO create(UserRequestDTO dto) {
+        User user = userMapper.toEntity(dto);
+        return userMapper.toResponseDTO(userRepository.save(user));
+    }
+
+    public UserResponseDTO update(UUID id, UserUpdateDTO dto) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado!"));
+        userMapper.updateEntityFromDTO(dto, user);
+        return userMapper.toResponseDTO(userRepository.save(user));
+    }
+}
