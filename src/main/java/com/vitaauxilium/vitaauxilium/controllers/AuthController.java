190291c7cbf6e5421f4d594b59672632872c22f3
Ryan-Barros <ryanbarros975@gmail.com>
2026-05-09 19:23:00 -0300

    Implementando controller de autenticação

---
+++ src/main/java/com/vitaauxilium/vitaauxilium/controllers/AuthController.java
@@ -0,0 +1,25 @@
+package com.vitaauxilium.vitaauxilium.controllers;
+
+import com.vitaauxilium.vitaauxilium.dto.request.AuthRequestDTO;
+import com.vitaauxilium.vitaauxilium.dto.response.AuthResponseDTO;
+import com.vitaauxilium.vitaauxilium.services.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody @Valid AuthRequestDTO dto) {
+        return ResponseEntity.ok(authService.login(dto));
+    }
+}
