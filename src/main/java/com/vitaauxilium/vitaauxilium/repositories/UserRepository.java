38fc80925d89261533359618eea16951cdc5b51c
Ryan-Barros <ryanbarros975@gmail.com>
2026-05-13 19:04:34 -0300

    Implementa método findByName com unaccent

--- src/main/java/com/vitaauxilium/vitaauxilium/repositories/UserRepository.java
+++ src/main/java/com/vitaauxilium/vitaauxilium/repositories/UserRepository.java
@@ -2,11 +2,17 @@ package com.vitaauxilium.vitaauxilium.repositories;
 
 import com.vitaauxilium.vitaauxilium.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
+
+    @Query(value = "select * from users where unaccent('unaccent', name) ilike unaccent('unaccent', :name)", nativeQuery = true)
+    List<User> findByName(@Param("name") String name);
 }
--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,1 +1,3 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE EXTENSION IF NOT EXISTS "unaccent";
+CREATE INDEX ON users (unaccent('unaccent', name) text_pattern_ops);
