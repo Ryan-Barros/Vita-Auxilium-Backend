ee2e2822ad63f076d0fecc674f9dd7359b2a011a
Ryan-Barros <ryanbarros975@gmail.com>
2026-06-28 12:33:43 -0300

    Modifica o critério para Equals e Hashcode, e adiciona validações

--- src/main/java/com/vitaauxilium/vitaauxilium/models/DeviceData.java
+++ src/main/java/com/vitaauxilium/vitaauxilium/models/DeviceData.java
@@ -1,9 +1,7 @@
 package com.vitaauxilium.vitaauxilium.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.envers.Audited;
 
 import java.sql.Timestamp;
@@ -12,6 +10,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Audited
 @Entity
 @Table(name = "device_data")
@@ -19,53 +18,55 @@ public class DeviceData {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
     private UUID id;
 
-    @Column(name = "device_id", nullable = false)
-    private UUID deviceId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "device_id", nullable = false)
+    private Device deviceId;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int bpm;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int hrv;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private double temperature;
 
-    @Column(nullable = false)
-    private int oxigenation;
+    @Column(nullable = false, updatable = false)
+    private int oxygenation;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int accelerometer;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int gyroscope;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int movement;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int movement_level;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int fall;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int chance_of_fall;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int battery;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int charging;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int voltage;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private Timestamp date;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private int wifirssi;
 }
