890fd8efb4b90d5236247c19f7a70af251392b80
Ryan-Barros <ryanbarros975@gmail.com>
2026-06-28 22:40:22 -0300

    Renomeia entidade e adiciona atributos

--- src/main/java/com/vitaauxilium/vitaauxilium/models/Environment.java
+++ src/main/java/com/vitaauxilium/vitaauxilium/models/Environment.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import org.hibernate.envers.Audited;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -14,14 +16,17 @@ import java.util.UUID;
 @NoArgsConstructor
 @Audited
 @Entity
-@Table(name = "ambients")
-public class Ambient {
+@Table(name = "environments")
+public class Environment {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "ambient_code", nullable = false, unique = true)
-    private String ambientCode;
+    @Column(name = "environment_name", nullable = false)
+    private String environmentName;
+
+    @Column(name = "environment_code", nullable = false, unique = true)
+    private String environmentCode;
 
     @Column(name = "expiration_date", nullable = false)
     private LocalDateTime expirationDate;
@@ -31,4 +36,7 @@ public class Ambient {
 
     @Column(name = "is_active", nullable = false, columnDefinition = "true")
     private boolean isActive;
+
+    @OneToMany(mappedBy = "environment", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EnvironmentMember> members = new ArrayList<>();
 }
