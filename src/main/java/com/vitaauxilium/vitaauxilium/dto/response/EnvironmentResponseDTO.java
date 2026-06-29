8d9f0e05c21b256ba134fc5e90389b435d3a823b
Ryan-Barros <ryanbarros975@gmail.com>
2026-06-28 22:37:48 -0300

    Adiciona atributo EnvironmentName para o ResponseDTO

--- src/main/java/com/vitaauxilium/vitaauxilium/dto/response/EnvironmentResponseDTO.java
+++ src/main/java/com/vitaauxilium/vitaauxilium/dto/response/EnvironmentResponseDTO.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 
 public record EnvironmentResponseDTO(
     UUID id,
+    String environmentName,
     String environmentCode,
     LocalDateTime expirationDate,
     String activeCode,
