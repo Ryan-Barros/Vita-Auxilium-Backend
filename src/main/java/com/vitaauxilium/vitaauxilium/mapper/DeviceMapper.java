8cf43704571ea1b0dfac607612ab5f074c934da8
Ryan-Barros <ryanbarros975@gmail.com>
2026-07-11 15:06:23 -0300

    Adiciona métodos ao mapper

--- src/main/java/com/vitaauxilium/vitaauxilium/mapper/DeviceMapper.java
+++ src/main/java/com/vitaauxilium/vitaauxilium/mapper/DeviceMapper.java
@@ -1,7 +1,17 @@
 package com.vitaauxilium.vitaauxilium.mapper;
 
+import com.vitaauxilium.vitaauxilium.dto.request.DeviceRequestDTO;
+import com.vitaauxilium.vitaauxilium.dto.response.DeviceResponseDTO;
+import com.vitaauxilium.vitaauxilium.models.Device;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface DeviceMapper {
+    DeviceResponseDTO toResponseDTO(Device device);
+
+    List<DeviceResponseDTO> toResponseDTOList(List<Device> devices);
+
+    Device toEntity(DeviceRequestDTO dto);
 }
