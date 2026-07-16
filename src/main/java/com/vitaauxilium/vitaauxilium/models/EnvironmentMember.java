8dce0494be5e0183a552ee0ec4b391b27f7f964b
Ryan-Barros <ryanbarros975@gmail.com>
2026-07-16 11:27:24 -0300

    Adiciona entidade intermediária EnvironmentMember

---
+++ src/main/java/com/vitaauxilium/vitaauxilium/models/EnvironmentMember.java
@@ -0,0 +1,32 @@
+package com.vitaauxilium.vitaauxilium.models;
+
+import com.vitaauxilium.vitaauxilium.config.GeneratedUuidV7;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Audited
+@Entity
+@Table(name = "environment_members")
+public class EnvironmentMember {
+
+    @Id
+    @GeneratedUuidV7
+    @Column(name = "environment_member_id", updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_user_id", nullable = false, updatable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "member_environment_id", nullable = false, updatable = false)
+    private Environment environment;
+}
