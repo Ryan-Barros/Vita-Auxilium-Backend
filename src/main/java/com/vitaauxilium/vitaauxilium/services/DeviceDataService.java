9bf7bd8102e5b3d5b938e7258cdaf6089ff885a1
Ryan-Barros <ryanbarros975@gmail.com>
2026-07-16 11:17:05 -0300

    Adiciona service para DeviceData

---
+++ src/main/java/com/vitaauxilium/vitaauxilium/services/DeviceDataService.java
@@ -0,0 +1,41 @@
+package com.vitaauxilium.vitaauxilium.services;
+
+import com.vitaauxilium.vitaauxilium.dto.response.DeviceDataResponseDTO;
+import com.vitaauxilium.vitaauxilium.mapper.DeviceDataMapper;
+import com.vitaauxilium.vitaauxilium.models.DeviceData;
+import com.vitaauxilium.vitaauxilium.repositories.DeviceDataRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class DeviceDataService {
+
+    private final DeviceDataRepository deviceDataRepository;
+    private final DeviceDataMapper deviceDataMapper;
+
+    public List<DeviceDataResponseDTO> findAll() {
+        List<DeviceData> dataList = deviceDataRepository.findAll();
+        return deviceDataMapper.toResponseDTOList(dataList);
+    }
+
+    public DeviceData findById(UUID id) {
+        return deviceDataRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Dados não encontrados"));
+    }
+
+    public void create(DeviceData data) {
+        deviceDataRepository.save(data);
+    }
+
+    public void delete(UUID id) {
+        DeviceData data = deviceDataRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Dados não encontrados"));
+        deviceDataRepository.delete(data);
+    }
+
+}
