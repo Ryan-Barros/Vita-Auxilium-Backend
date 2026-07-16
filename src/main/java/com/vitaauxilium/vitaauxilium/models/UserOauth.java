41d08a71da79c730b04c4c43a33584ae48db4db9
Ryan-Barros <ryanbarros975@gmail.com>
2026-07-16 11:43:56 -0300

    Adiciona entidade UserOauth

---
+++ src/main/java/com/vitaauxilium/vitaauxilium/models/UserOauth.java
@@ -0,0 +1,39 @@
+package com.vitaauxilium.vitaauxilium.models;
+
+import com.vitaauxilium.vitaauxilium.config.GeneratedUuidV7;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users_oauth")
+@EntityListeners(AuditingEntityListener.class)
+public class UserOauth {
+
+    @Id
+    @GeneratedUuidV7
+    @Column(name = "oauth_id", nullable = false, updatable = false)
+    private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "oauth_user_id", nullable = false)
+    private User oauthUser;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "oauth_provider", nullable = false)
+    private Provider oauthProvider;
+
+    @Column(name = "created_at", nullable = false)
+    @CreatedDate
+    private Instant creationDate;
+}
