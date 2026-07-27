637c8dfe5e3a2c307be777cef923c5a357871ddf
Ryan-Barros <ryanbarros975@gmail.com>
2026-07-27 12:02:07 -0300

    Modifica anotações e adiciona validações de campos

--- src/main/java/com/vitaauxilium/vitaauxilium/dto/request/UserRequestDTO.java
+++ src/main/java/com/vitaauxilium/vitaauxilium/dto/request/UserRequestDTO.java
@@ -1,20 +1,28 @@
 package com.vitaauxilium.vitaauxilium.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.vitaauxilium.vitaauxilium.models.Profile;
+import jakarta.validation.constraints.*;
 
 public record UserRequestDTO(
         @NotBlank(message = "Nome é obrigatório")
+        @Size(min = 3, message = "Nome muito curto")
         String name,
 
         @NotBlank(message = "Número de telefone é obrigatório")
+        @Pattern(regexp = "^\\d{10,11}$", message = "Formato de telefone deve conter 10 ou 11 dígitos")
         String phone,
+
+        @NotNull(message = "Perfil é obrigatório")
+        @JsonAlias({"profile"})
+        Profile userProfile,
 
         @Email(message = "E-mail inválido")
         @NotBlank(message = "E-mail é obrigatório")
         String email,
 
+        @NotBlank
         @Size(min = 6, message = "A senha deve conter no mínimo 6 caracteres")
         String password
-) {}
+) {
+}
