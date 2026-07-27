22696734a33907bd11ec117cee8a3367a5df985d
Ryan-Barros <ryanbarros975@gmail.com>
2026-07-27 12:23:45 -0300

    Corrige e adiciona validações

--- src/main/java/com/vitaauxilium/vitaauxilium/dto/request/DeviceRequestDTO.java
+++ src/main/java/com/vitaauxilium/vitaauxilium/dto/request/DeviceRequestDTO.java
@@ -1,16 +1,21 @@
 package com.vitaauxilium.vitaauxilium.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 public record DeviceRequestDTO(
-    @NotBlank(message = "ID do ambiente não pode ser nulo")
-    UUID environmentId,
+        @NotNull(message = "ID do ambiente não pode ser nulo")
+        UUID environmentId,
 
-    @NotBlank(message = "O nome do dispositivo não deve ser nulo")
-    String name,
+        @NotBlank(message = "O nome do dispositivo não deve ser nulo")
+        @Size(min = 4, message = "Nome muito curto")
+        String name,
 
-    @NotBlank(message = "O token do dispositivo não deve ser nulo")
-    String tokenHash
-) {}
+        @NotBlank(message = "O token do dispositivo não deve ser nulo")
+        @Size(min = 13, max = 13, message = "Formato de token incorreto")
+        String tokenHash
+) {
+}
--- src/main/java/com/vitaauxilium/vitaauxilium/dto/request/EnvironmentRequestDTO.java
+++ src/main/java/com/vitaauxilium/vitaauxilium/dto/request/EnvironmentRequestDTO.java
@@ -1,8 +1,11 @@
 package com.vitaauxilium.vitaauxilium.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record EnvironmentRequestDTO(
-   @NotNull(message = "Nome do ambiente é obrigatório!")
-   String environmentName
-) {}
+        @NotBlank(message = "Nome do ambiente é obrigatório!")
+        @Size(min = 5, message = "Nome muito curto")
+        String environmentName
+) {
+}
