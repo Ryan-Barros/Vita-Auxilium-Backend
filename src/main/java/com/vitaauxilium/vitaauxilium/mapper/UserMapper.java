b9b1e3b63c9d7bebeb20822cd02495adbd5eaedc
Ryan-Barros <ryanbarros975@gmail.com>
2026-08-24 19:11:28 -0300

    Altera politica de mapper e marca atributos que devem ser ignorados

--- src/main/java/com/vitaauxilium/vitaauxilium/mapper/UserMapper.java
+++ src/main/java/com/vitaauxilium/vitaauxilium/mapper/UserMapper.java
@@ -8,10 +8,17 @@ import org.mapstruct.*;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface UserMapper {
     UserResponseDTO toResponseDTO(User user);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "picture", ignore = true)
+    @Mapping(target = "cpf", ignore = true)
+    @Mapping(target = "active", ignore = true)
+    @Mapping(target = "activeAmbient", ignore = true)
+    @Mapping(target = "environments", ignore = true)
+    @Mapping(target = "authorities", ignore = true)
     User toEntity(UserRequestDTO dto);
 
     List<UserResponseDTO> toResponseDTOList(List<User> users);
@@ -20,5 +27,13 @@ public interface UserMapper {
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntityFromDTO(User source, @MappingTarget User target);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "picture", ignore = true)
+    @Mapping(target = "cpf", ignore = true)
+    @Mapping(target = "active", ignore = true)
+    @Mapping(target = "activeAmbient", ignore = true)
+    @Mapping(target = "environments", ignore = true)
+    @Mapping(target = "authorities", ignore = true)
+    @Mapping(target = "profile", ignore = true)
     User toEntityFromUpdateDTO(UserUpdateDTO dto);
 }
